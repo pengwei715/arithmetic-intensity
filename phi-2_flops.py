bec8ea90351bbc3a00df44dc189885353019d378
peng wei <pengwei715@gmail.com>
2024-02-07 13:29:01 -0800

    added phi2

--- phi-2_flops.py
+++ phi-2_flops.py
@@ -7,14 +7,20 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from fvcore.nn import flop_count_table
 from fvcore.nn import flop_count_str
-
+import numpy as np
+import torch
 torch.set_default_device("cpu")
 
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1", torch_dtype=torch.float32, trust_remote_code=True)
+
+print(model)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 input_text = "Please give me a detailed information about the capital of France."
 
 input_tokens = tokenizer(input_text, return_tensors="pt").input_ids
+#print(input_tokens)
+
+input_tokens = torch.tensor(np.array([[1]]))
 flops = FlopCountAnalysis(model, input_tokens)
 
 
